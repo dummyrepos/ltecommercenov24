9c76706bc233a8389837d95ff5e19fa5504244bf
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2024-11-24 08:43:23 +0530

    task_1002: setup basic structure

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""This is the main application class
+Which contains version and main function
+"""
+__version__ = "0.1.0"
+
+def main():
+    """This is the main function"""
+    print("Hello World!")
+    print("This is the main function")
+    print("This is the main function")
+    print("This is the main function")
+
+def get_version():
+    """This function returns the version of the application"""
+    return __version__
+
+if __name__ == "__main__":
+    main()
