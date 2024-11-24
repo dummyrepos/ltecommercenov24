9f210f70136cefb0a6c65316075881fd31f21ea1
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2024-11-24 08:45:34 +0530

    task_1003:  create app code

--- src/main.py
+++ src/main.py
@@ -1,18 +1,1 @@
-"""This is the main application class
-Which contains version and main function
-"""
-__version__ = "0.1.0"
-
-def main():
-    """This is the main function"""
-    print("Hello World!")
-    print("This is the main function")
-    print("This is the main function")
-    print("This is the main function")
-
-def get_version():
-    """This function returns the version of the application"""
-    return __version__
-
-if __name__ == "__main__":
-    main()
+# TBD: primary code needs to be added
