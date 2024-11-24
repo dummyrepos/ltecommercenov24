9df02c7bcd7cb8ecb0c81d836232ad89ef2e435c
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2024-11-24 09:18:36 +0530

    Added main function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-# TBD: primary code needs to be added
+"""This is the main module
+
+This module will have main function and version info
+"""
+__version__ = "0.1.0"
+
+def main():
+    """This is the main function
+
+    This function will print version info
+    """
+    print(f"Version: {__version__}")
+
+if __name__ == "__main__":
+    main()
